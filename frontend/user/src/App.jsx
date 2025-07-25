3baab217c6c271e80b8b3a7401f0894dc133ad24
citronoiseau <avroraka2004@gmail.com>
2025-07-25 14:31:43 +0000

    fixed css

--- frontend/user/src/App.jsx
+++ frontend/user/src/App.jsx
@@ -2,8 +2,6 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer";
 
-import "./App.css";
-
 function App() {
   return (
     <div className="app">
